d77f5237b5a5003aa1c93f0da52c6bfb2f9680b8
Snigdha <snigdha.sinha@utexas.edu>
2020-07-25 22:20:19 -0500

    new starter

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,27 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const Menu = (props) => (
+const Menu = props => {
+  const [sub, setSub] = useState(null)
+  const items = [
+    { id: 'Home', to: '/' },
+    { id: 'About Us', to: '/about' },
+    {
+      id: 'Education',
+      to: '/education',
+      subitems: [
+        { id: 'Foundations', to: '/education/foundations' },
+        { id: 'History', to: '/education/history' },
+        {
+          id: 'Emotional Intelligence',
+          to: '/education/emotional-intelligence',
+        },
+        { id: 'Resources', to: '/education/resources' },
+      ],
+    },
+    { id: 'Personal Stories', to: '/personal-stories' },
+    { id: 'Take Action', to: '/take-action' },
+    { id: 'Contact Us', to: '/contact' },
+  ]
+
+  const handleSub = id => {
+    const item = items.filter(item => item.id === id)
+    console.log(item[0].subitems)
+    setSub(item[0].subitems)
+    // setSub(id)
+  }
+  //   console.log(sub)
+  return (
     <nav id="menu">
-        <div className="inner">
+      <div className="inner">
+        <div className="grid-wrapper">
+          <div className="col-6">
             <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+              {items.map(item => {
+                return (
+                  <li key={item.id}>
+                    <Link
+                      id={item.id}
+                      onClick={props.onToggleMenu}
+                      onMouseOver={event => handleSub(event.target.id)}
+                      to={item.to}
+                    >
+                      {item.id}
+                      {item.subitems ? '+' : ''}
+                    </Link>
+                  </li>
+                )
+              })}
+              {/* {sub === 'Home' ? (
+                <ul className="sub-item">
+                  <li>
+                    <Link onClick={props.onToggleMenu} to="/">
+                      Hi
+                    </Link>
+                  </li>
+                  <li>
+                    <Link onClick={props.onToggleMenu} to="/">
+                      Testing
+                    </Link>
+                  </li>
+                </ul>
+              ) : null} */}
+              <li>
+                <Link onClick={props.onToggleMenu} to="/generic">
+                  Generic
+                </Link>
+              </li>
+              <li>
+                <Link onClick={props.onToggleMenu} to="/elements">
+                  Elements
+                </Link>
+              </li>
             </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
+          </div>
+          <div className="col-6">
+            <ul className="sub-item">
+              {sub
+                ? sub.map(s => {
+                    return (
+                      <li key={s.id}>
+                        <Link onClick={props.onToggleMenu} to={s.to}>
+                          {s.id}
+                        </Link>
+                      </li>
+                    )
+                  })
+                : null}
             </ul>
+          </div>
         </div>
-        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
+        <ul className="actions vertical">
+          <li>
+            <a href="#" className="button special fit">
+              Get Started
+            </a>
+          </li>
+          <li>
+            <a href="#" className="button fit">
+              Log In
+            </a>
+          </li>
+        </ul>
+        <a className="close" onClick={props.onToggleMenu} href="javascript:;">
+          Close
+        </a>
+      </div>
     </nav>
-)
+  )
+}
 
 Menu.propTypes = {
-    onToggleMenu: PropTypes.func
+  onToggleMenu: PropTypes.func,
 }
 
 export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,84 +10,138 @@ import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
 import pic05 from '../assets/images/pic05.jpg'
 import pic06 from '../assets/images/pic06.jpg'
+import pic09 from '../assets/images/pic09.jpg'
+import pic10 from '../assets/images/pic10.jpg'
 
 class HomeIndex extends React.Component {
-    render() {
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          title="The Chameleon Effect"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        ></Helmet>
 
-        return (
-            <Layout>
-                <Helmet
-                    title="Gatsby Starter - Forty"
-                    meta={[
-                        { name: 'description', content: 'Sample' },
-                        { name: 'keywords', content: 'sample, something' },
-                    ]}
-                >
-                </Helmet>
-
-                <Banner />
-
-                <div id="main">
-                    <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${pic01})`}}>
-                            <header className="major">
-                                <h3>Aliquam</h3>
-                                <p>Ipsum dolor sit amet</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic02})`}}>
-                            <header className="major">
-                                <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic03})`}}>
-                            <header className="major">
-                                <h3>Magna</h3>
-                                <p>Lorem etiam nullam</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
-                            <header className="major">
-                                <h3>Ipsum</h3>
-                                <p>Nisl sed aliquam</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic05})`}}>
-                            <header className="major">
-                                <h3>Consequat</h3>
-                                <p>Ipsum dolor sit amet</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic06})`}}>
-                            <header className="major">
-                                <h3>Etiam</h3>
-                                <p>Feugiat amet tempus</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                    </section>
-                    <section id="two">
-                        <div className="inner">
-                            <header className="major">
-                                <h2>Massa libero</h2>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
-                            </ul>
-                        </div>
-                    </section>
+        <Banner />
+        <div id="main" className="alt">
+          <section id="one">
+            {/* <div id="main"> */}
+            <div className="inner">
+              <h2 id="content">Project Summary</h2>
+              <p>
+                Praesent ac adipiscing ullamcorper semper ut amet ac risus.
+                Lorem sapien ut odio odio nunc. Ac adipiscing nibh porttitor
+                erat risus justo adipiscing adipiscing amet placerat accumsan.
+                Vis. Faucibus odio magna tempus adipiscing a non. In mi primis
+                arcu ut non accumsan vivamus ac blandit adipiscing adipiscing
+                arcu metus praesent turpis eu ac lacinia nunc ac commodo gravida
+                adipiscing eget accumsan ac nunc adipiscing adipiscing.
+              </p>
+            </div>
+          </section>
+          <section id="two">
+            <div className="inner">
+              <header className="major">
+                <h2>Meet the Team</h2>
+              </header>
+              <div className="box alt">
+                <div className="grid-wrapper">
+                  <div className="col-4">
+                    <span className="image fit">
+                      <img src={pic09} alt="" />
+                      <p>hi</p>
+                    </span>
+                  </div>
+                  <div className="col-4">
+                    <span className="image fit">
+                      <img src={pic10} alt="" />
+                    </span>
+                  </div>
+                  <div className="col-4">
+                    <span className="image fit">
+                      <img src={pic10} alt="" />
+                    </span>
+                  </div>
                 </div>
-
-            </Layout>
-        )
-    }
+              </div>
+            </div>
+          </section>
+          {/* <div id="main">
+            <section id="one" className="pics">
+              <article style={{ backgroundImage: `url(${pic01})` }}>
+                <header className="major">
+                  <h3>Aliquam</h3>
+                  <p>Ipsum dolor sit amet</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+              <article style={{ backgroundImage: `url(${pic02})` }}>
+                <header className="major">
+                  <h3>Tempus</h3>
+                  <p>feugiat amet tempus</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+              <article style={{ backgroundImage: `url(${pic03})` }}>
+                <header className="major">
+                  <h3>Magna</h3>
+                  <p>Lorem etiam nullam</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+              <article style={{ backgroundImage: `url(${pic04})` }}>
+                <header className="major">
+                  <h3>Ipsum</h3>
+                  <p>Nisl sed aliquam</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+              <article style={{ backgroundImage: `url(${pic05})` }}>
+                <header className="major">
+                  <h3>Consequat</h3>
+                  <p>Ipsum dolor sit amet</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+              <article style={{ backgroundImage: `url(${pic06})` }}>
+                <header className="major">
+                  <h3>Etiam</h3>
+                  <p>Feugiat amet tempus</p>
+                </header>
+                <Link to="/landing" className="link primary"></Link>
+              </article>
+            </section>
+            <section id="two">
+              <div className="inner">
+                <header className="major">
+                  <h2>Massa libero</h2>
+                </header>
+                <p>
+                  Nullam et orci eu lorem consequat tincidunt vivamus et
+                  sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
+                  pharetra. Pellentesque condimentum sem. In efficitur ligula
+                  tate urna. Maecenas laoreet massa vel lacinia pellentesque
+                  lorem ipsum dolor. Nullam et orci eu lorem consequat
+                  tincidunt. Vivamus et sagittis libero. Mauris aliquet magna
+                  magna sed nunc rhoncus amet pharetra et feugiat tempus.
+                </p>
+                <ul className="actions">
+                  <li>
+                    <Link to="/landing" className="button next">
+                      Get Started
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div> */}
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
